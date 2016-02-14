f927acb1c9ccbc57a14c43d1688f2b964a3fe637
collybaby <colleengeorge111@gmail.com>
2016-02-13 23:16:36 -0800

    first commit

--- js/app.js
+++ js/app.js
@@ -1,26 +1,2 @@
-$(function(){ 
-  var $newButton = $('#newButton');
-  var $newForm = $('#newForm');
-  var $textInput = $('input:text');
-
-  $newButton.show();
-  $newForm.hide();
-
-
-$('#showForm'.on('click', function(){
-  $newButton.hide();
-  $newForm.show();
-});
-
-$newForm.on('submit', function(e){
-  e.preventDefault();
-  var newText = $('input:text').val();
-  $('li:last').after('<li>' + newText '</li>');
-  $newForm.hide();
-  $newButton.show();
-  $textInput.val('');
-});
-
-});
 
 
