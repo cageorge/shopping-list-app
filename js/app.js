bd872869d64bcfbb5c5b4d473a78673c2559caef
collybaby <colleengeorge111@gmail.com>
2016-02-13 19:55:39 -0800

    first commit

--- js/app.js
+++ js/app.js
@@ -1,43 +1,26 @@
-$(document).ready(function() {
-  $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
-  })
-  .mouseleave(function() {
-    $('.ryu-ready').hide();
-    $('.ryu-still').show();
-  })
-  .mousedown(function() {
-    // play hadouken sound
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').show().animate(
-    {'left': '1020px'},
-    500,
-    function() {
-    $(this).hide();
-    $(this).css('left', '520px');
-  });
-})
-    .mouseup(function(){
-        $('.ryu-throwing').hide();
-        $('.ryu-ready').show();
-  })
+$(function(){ 
+  var $newButton = $('#newButton');
+  var $newForm = $('#newForm');
+  var $textInput = $('input:text');
+
+  $newButton.show();
+  $newForm.hide();
+
+
+$('#showForm'.on('click', function(){
+  $newButton.hide();
+  $newForm.show();
 });
-  $(document).keydown(function(e) {
-    if (e.keyCode == 88) 
-      $('.ryu-ready').hide();
-      $('.ryu-cool').show();
-    })
-  $(document).keyup(function(e) {
-    if (e.keyCode == 88) 
-      $('.ryu-cool').hide();
-      $('.ryu-ready').show();
-    })      
-  
-function playHadouken() {
-    $('#hadouken-sound')[0].volume = 0.5;
-    $('#hadouken-sound')[0].load();
-    $('#hadouken-sound')[0].play();
-}
+
+$newForm.on('submit', function(e){
+  e.preventDefault();
+  var newText = $('input:text').val();
+  $('li:last').after('<li>' + newText '</li>');
+  $newForm.hide();
+  $newButton.show();
+  $textInput.val('');
+});
+
+});
+
 
