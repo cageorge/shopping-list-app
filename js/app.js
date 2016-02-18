fc436d9cd0d2071a483de826602f2526f8559bf6
Patrick Ford <patrickford@msn.com>
2016-02-17 20:43:49 -0800

    Fixed some layout

--- js/app.js
+++ js/app.js
@@ -3,19 +3,27 @@ $(document).ready(function() {
   $("#formSubmit").click(function() {
     var newItem = $("#itemInput").val();
     $("#itemInput").val("");
-    $("#list").append("<li><input type='checkbox'id='checkbox'/>" + newItem + 
-      "<button class='remove'>x</button></li>");
+    $("#list").append("<li><input type='checkbox' class='checkbox'/><span>" + newItem + 
+      "</span><button class='remove'>x</button></li>");
     return false;
   })
-
-})
+});
 
 $(document).on('click', '.remove', function() {
   $(this).parent().remove();
-})
-$(document).on('click', '#checkbox', function(){
-  $(this).parent().css('text-decoration', "line-through");
-})
+});
 
+// $(document).on('click', '.checkbox', function(){
+//   console.log($(this));
+//   $(this).next().css('text-decoration', "line-through");
+// });
+
+$(document).on('click', "input[type='checkbox']", function() {
+  if (this.checked) {
+    $(this).next().addClass("strikethrough");
+  } else if (!this.checked) {
+    $(this).next().removeClass("strikethrough");
+  }
+});
 
 
